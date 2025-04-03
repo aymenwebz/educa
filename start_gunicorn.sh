9c3ecb5c4c83232855e1cd7b00e66feae380faaa
aymenwebz <shatterlineyt@gmail.com>
2025-04-03 14:03:00 +0100

    Update start_gunicorn.sh

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /opt/render/project/src/educa
+cd /opt/render/project/src
 source /opt/render/project/src/env/educa/bin/activate
 exec gunicorn --access-logfile - --workers 3 --bind unix:/opt/render/project/src/educa/educa.sock educa.wsgi:application
